fa21615ac23acc1e636b92eaa892597ff2804048
Nick Spain <nicholas.spain96@gmail.com>
2022-12-20 21:21:15 +1100

    Initial Commit

---
+++ urbanclimb-froth/urbanclimb_frothApp.swift
@@ -0,0 +1,17 @@
+//
+//  urbanclimb_frothApp.swift
+//  urbanclimb-froth
+//
+//  Created by Nick Spain on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct urbanclimb_frothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ urbanclimb-frothUITests/urbanclimb_frothUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  urbanclimb_frothUITestsLaunchTests.swift
+//  urbanclimb-frothUITests
+//
+//  Created by Nick Spain on 20/12/2022.
+//
+
+import XCTest
+
+final class urbanclimb_frothUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
