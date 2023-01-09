7d4448be69591d0cbb7b231b4960e15513e74229
Nick Spain <nicholas.spain96@gmail.com>
2023-01-09 21:31:21 +1100

    Working menubar app

--- urbanclimb-froth/urbanclimb_frothApp.swift
+++ urbanclimb-froth/urbanclimb_frothApp.swift
@@ -1,98 +1,85 @@
-//
-//  urbanclimb_frothApp.swift
-//  urbanclimb-froth
-//
-//  Created by Nick Spain on 20/12/2022.
-//
-
 import SwiftUI
 
 @main
 struct urbanclimb_frothApp: App {
-    @State var currentVenue: String = "Collingwood"
-    @State var collingwoodStatus = VenueStatus(name: "Collingwood", status: "unknown", froth: "unknown")
-    @State var backgroundUpdater: DispatchWorkItem? = nil
-    @State var updated: Bool = false
-    
-    init() {
-        NSLog("Starting updater thread (current thread: \(Thread.current)")
-        self.ensureBackgroundFrothUpdate()
-    }
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     
     var body: some Scene {
-        MenuBarExtra("UrbanClimb Froth", systemImage: "figure.climbing") {
-            Button(collingwoodStatus.to_s()) {
-                NSLog("Collingwood, status: \(collingwoodStatus), updated: \(updated) (current thread: \(Thread.current)")
-                currentVenue = "Collingwood"
-            }
-        }
-    }
-    
-    func updateFrothStatus() async {
-        NSLog("Updating froth status")
-    }
-    
-    func getVenueStatus(venue: String, completion: @escaping (_ venueStatus: VenueStatusResponse) -> Void) {
-        var uuid: String? = nil
-        if venue == "Collingwood" {
-            uuid = "8674E350-D340-4AB3-A462-5595061A6950"
-        } else {
-            uuid = "46E5373C-2310-4520-B576-CCB4E4EF548D"
-        }
-        let url = URL(string: "https://portal.urbanclimb.com.au/uc-services/ajax/gym/occupancy.ashx?branch=\(uuid!)")
-        let task = URLSession.shared.dataTask(with: url!) {
-            (data, response, error) in
-            if let error = error {
-                NSLog("Error: \(error)")
-                return
-            }
-            let decoder = JSONDecoder()
-            do {
-                let body = try decoder.decode(VenueStatusResponse.self, from: data!)
-                completion(body)
-            } catch {
-                NSLog("Deserialization failed: \(error)")
-            }
-            
-        }
-        task.resume()
-    }
-    
-    func ensureBackgroundFrothUpdate() {
-        if (self.backgroundUpdater == nil) {
-            NSLog("No background updater found, creating one")
-            let backgroundUpdater = DispatchWorkItem {
-                NSLog("Background updater started")
-                while true {
-                    let work = DispatchWorkItem {
-                        NSLog("Updating the status for each of our venues (current thread: \(Thread.current)")
-                        getVenueStatus(venue: "Collingwood") {
-                            response in
-                                DispatchQueue.main.async {
-                                    NSLog("Updating collingwood status on main thread to \(response) (current thread: \(Thread.current)")
-                                    self.collingwoodStatus = VenueStatus.fromResponse(response: response)
-                                    self.updated.toggle()
-                                    NSLog("Updated collingwood status: \(self.collingwoodStatus.to_s()) (current thread: \(Thread.current)")
-                                }
-                        }
-                    }
-                    DispatchQueue.global().asyncAfter(deadline: .now() + 10, execute: work)
-                    work.wait()
-                    }
-                }
-            
-            self.backgroundUpdater = backgroundUpdater
-            NSLog("Putting background updater on the global dispatch queue")
-            DispatchQueue.global().async(execute: backgroundUpdater)
-            
+        WindowGroup {
+            ContentView(vm: VenueStatusListViewModel())
         }
     }
 }
 
-struct VenueStatusResponse: Codable {
-    var name: String
-    var status: String
-    var froth: String
+class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    private var venueStatusListVM: VenueStatusListViewModel!
+    
+    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
+        self.venueStatusListVM = VenueStatusListViewModel()
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(
+                systemSymbolName: "figure.climbing",
+                accessibilityDescription: "Climbing"
+            )
+            statusButton.action = #selector(togglePopover)
+        }
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 300, height: 300)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: ContentView(vm: self.venueStatusListVM))
+    }
+    
+    @objc func togglePopover() {
+        Task {
+            await self.venueStatusListVM.populateVenueStatus()
+        }
+        
+        if let button = self.statusItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(nil)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @StateObject private var vm: VenueStatusListViewModel
+    
+    init(vm: VenueStatusListViewModel) {
+        self._vm = StateObject(wrappedValue: vm)
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Urban Climb Froth").padding()
+            List(vm.venueStatuss, id: \.name) { venue in
+                HStack(alignment: .center) {
+                    Text(venue.name).fontWeight(.semibold)
+                    Text("(\(venue.status)): ")
+                    Text(venue.froth)
+                }
+            }.task {
+                await vm.populateVenueStatus()
+            }
+        }.frame(width: 300, height: 300)
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView(vm: VenueStatusListViewModel())
+    }
+}
+
+struct VenueStatus: Codable {
+    let name: String
+    let status: String
+    let froth: String
     
     enum CodingKeys: String, CodingKey {
         case name = "Name"
@@ -101,23 +88,63 @@ struct VenueStatusResponse: Codable {
     }
 }
 
-class VenueStatus {
-    var name: String
-    var status: String
-    var froth: String
-    
-    init(name: String, status: String, froth: String) {
-        self.name = name
-        self.status = status
-        self.froth = froth
-    }
-    
-    func to_s() -> String {
-        return "\(name) (\(status)) [\(froth)]"
-    }
-    
-    static func fromResponse(response: VenueStatusResponse) -> VenueStatus {
-        return VenueStatus(name: response.name, status: response.status, froth: response.froth)
+enum NetworkError: Error {
+    case invalidResponse
+}
+
+class UrbanClimbFrothService {
+    func getVenueStatuss() async throws -> [VenueStatus] {
+        let venues = ["Collingwood"]
+        let venueUUIDs = [
+            "Collingwood": "8674E350-D340-4AB3-A462-5595061A6950",
+        ]
+        var statuss: [VenueStatus] = []
+        for venue in venues {
+            if let uuid = venueUUIDs[venue] {
+                let url = URL(string: "https://portal.urbanclimb.com.au/uc-services/ajax/gym/occupancy.ashx?branch=\(uuid)")
+                let (data, response) = try await URLSession.shared.data(from: url!)
+                guard let httpResponse = response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else {
+                    throw NetworkError.invalidResponse
+                }
+                let status = try JSONDecoder().decode(VenueStatus.self, from: data)
+                statuss.append(status)
+            }
+        }
+        return statuss
     }
 }
 
+@MainActor
+class VenueStatusListViewModel: ObservableObject {
+    @Published var venueStatuss: [VenueStatusViewModel] = []
+    
+    func populateVenueStatus() async {
+        do {
+            let statuss = try await UrbanClimbFrothService().getVenueStatuss()
+            self.venueStatuss = statuss.map(VenueStatusViewModel.init)
+        } catch {
+            print(error)
+        }
+    }
+}
+
+struct VenueStatusViewModel {
+    private var venueStatus: VenueStatus
+    
+    init(venueStatus: VenueStatus) {
+        self.venueStatus = venueStatus
+    }
+    
+    var name: String {
+        venueStatus.name
+    }
+    
+    var status: String {
+        venueStatus.status
+    }
+    
+    var froth: String {
+        venueStatus.froth
+    }
+}
